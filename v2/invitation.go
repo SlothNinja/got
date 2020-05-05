b382df8a1aa9699b6ee9bea3a0f70c517e76a0dd
SlothNinja <slothninja2@gmail.com>
2020-05-04 22:33:11 -0400

    undo features working

---
+++ v2/invitation.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+
+	"cloud.google.com/go/datastore"
+	"github.com/Pallinder/go-randomdata"
+	"github.com/SlothNinja/sn/v2"
+	"github.com/SlothNinja/user/v2"
+	"github.com/gin-gonic/gin"
+)
+
+// Invitation provides a game invitation
+type Invitation struct {
+	Key *datastore.Key `json:"key" datastore:"__key__"`
+	Header
+}
+
+func newInvitation(id int64) *Invitation {
+	return &Invitation{Key: newInvitationKey(id)}
+}
+
+func newInvitationKey(id int64) *datastore.Key {
+	return datastore.IDKey(invitationKind, id, rootKey(id))
+}
+
+func (inv *Invitation) Load(ps []datastore.Property) error {
+	return datastore.LoadStruct(inv, ps)
+}
+
+func (inv *Invitation) Save() ([]datastore.Property, error) {
+	t := time.Now()
+	if inv.CreatedAt.IsZero() {
+		inv.CreatedAt = t
+	}
+	inv.UpdatedAt = t
+	return datastore.SaveStruct(inv)
+}
+
+func (inv *Invitation) LoadKey(k *datastore.Key) error {
+	inv.Key = k
+	return nil
+}
+
+func defaultInvitation() *Invitation {
+	inv := newInvitation(0)
+
+	// Default Values
+	inv.Title = randomdata.SillyName()
+	inv.NumPlayers = 2
+	inv.TwoThiefVariant = false
+	return inv
+}
+
+func getID(c *gin.Context) (int64, error) {
+	return strconv.ParseInt(c.Param(idParam), 10, 64)
+}
+
+type omit *struct{}
+
+func (inv Invitation) MarshalJSON() ([]byte, error) {
+	type JInvitation Invitation
+
+	return json.Marshal(struct {
+		JInvitation
+		ID           int64   `json:"id"`
+		Creator      *User   `json:"creator"`
+		Users        []*User `json:"users"`
+		LastUpdated  string  `json:"lastUpdated"`
+		Public       bool    `json:"public"`
+		CreatorEmail omit    `json:"creatorEmail,omitempty"`
+		CreatorKey   omit    `json:"creatorKey,omitempty"`
+		CreatorName  omit    `json:"creatorName,omitempty"`
+		UserEmails   omit    `json:"userEmails,omitempty"`
+		UserKeys     omit    `json:"userKeys,omitempty"`
+		UserNames    omit    `json:"userNames,omitempty"`
+	}{
+		JInvitation: JInvitation(inv),
+		ID:          inv.Key.ID,
+		Creator:     toUser(inv.CreatorKey, inv.CreatorName, inv.CreatorEmail),
+		Users:       toUsers(inv.UserKeys, inv.UserNames, inv.UserEmails),
+		LastUpdated: sn.LastUpdated(inv.UpdatedAt),
+		Public:      inv.Password == "",
+	})
+}
+
+type User struct {
+	*user.User
+	ID        int64 `json:"id"`
+	LCName    omit  `json:"lcname,omitempty"`
+	Joined    omit  `json:"joined,omitempty"`
+	CreatedAt omit  `json:"createdat,omitempty"`
+	UpdatedAt omit  `json:"updatedat,omitempty"`
+	Admin     omit  `json:"admin,omitempty"`
+}
+
+func toUser(k *datastore.Key, name, email string) *User {
+	var id int64 = -1
+	if k != nil {
+		id = k.ID
+	}
+	u := &User{User: user.New(id)}
+	u.ID = id
+	u.Name = name
+	u.Email = email
+	return u
+}
+
+func toUsers(ks []*datastore.Key, names, emails []string) []*User {
+	us := make([]*User, len(ks))
+	for i := range ks {
+		us[i] = toUser(ks[i], names[i], emails[i])
+	}
+	return us
+}
---
+++ v2/src/components/invitation/Index.vue
@@ -0,0 +1,217 @@
+<template>
+  <v-app id='app'>
+    <sn-toolbar v-model='nav'></sn-toolbar>
+    <sn-nav-drawer v-model='nav' app></sn-nav-drawer>
+    <sn-snackbar v-model='snackbar.open'>
+      <div class='text-center'>
+        <span v-html='snackbar.message'></span>
+      </div>
+    </sn-snackbar>
+    <v-content>
+      <v-container>
+  <v-card>
+    <v-card-title primary-title>
+      <h3>Invitations</h3>
+    </v-card-title>
+    <v-card-text>
+      <v-data-table
+         :headers="headers"
+         :items="items"
+         >
+         <template v-slot:item.creator="{ item }">
+           <sn-user-btn :user="item.creator" size="x-small"></sn-user-btn>&nbsp;{{item.creator.name}}
+         </template>
+         <template v-slot:item.players="{ item }">
+           <div class="py-1" v-for="user in item.users" :key="user.id" >
+             <sn-user-btn :user="user" size="x-small"></sn-user-btn>&nbsp;{{user.name}}
+           </div>
+         </template>
+         <template v-slot:item.public="{ item }">
+           {{publicPrivate(item)}}
+         </template>
+         <template v-slot:item.actions="{ item }">
+           <v-btn 
+             x-small
+             rounded
+             width='62'
+             v-if="canAccept(item.id)"
+             @click="action('accept', item.id)"
+             color='info'
+             dark
+             >
+             Accept
+           </v-btn>
+           <v-btn 
+             x-small
+             rounded
+             width='62'
+             v-if="canDrop(item.id)"
+             @click="action('drop', item.id)"
+             color='info'
+             dark
+             >
+             Drop
+           </v-btn>
+         </template>
+      </v-data-table>
+    </v-card-text>
+  </v-card>
+      </v-container>
+    </v-content>
+    <sn-footer app></sn-footer>
+  </v-app>
+</template>
+
+<script>
+  import UserButton from '@/components/user/Button'
+  import Toolbar from '@/components/Toolbar'
+  import NavDrawer from '@/components/NavDrawer'
+  import Snackbar from '@/components/Snackbar'
+  import Footer from '@/components/Footer'
+
+  const _ = require('lodash')
+  const axios = require('axios')
+
+  export default {
+    name: 'index',
+    components: {
+      'sn-user-btn': UserButton,
+      'sn-toolbar': Toolbar,
+      'sn-nav-drawer': NavDrawer,
+      'sn-snackbar': Snackbar,
+      'sn-footer': Footer
+    },
+    data () {
+      return {
+        headers: [
+          {
+            text: 'ID',
+            align: 'left',
+            sortable: true,
+            value: 'id'
+          },
+          { text: 'Title', value: 'title' },
+          { text: 'Creator', value: 'creator' },
+          { text: 'Num Players', value: 'numPlayers' },
+          { text: 'Players', value: 'players' },
+          { text: 'Last Updated', value: 'lastUpdated' },
+          { text: 'Public/Private', value: 'public' },
+          { text: 'Actions', value: 'actions' }
+        ],
+        items: []
+      }
+    },
+    created () {
+      this.$root.toolbar = 'sn-toolbar'
+      this.fetchData()
+    },
+    watch: {
+      '$route': 'fetchData'
+    },
+    methods: {
+      fetchData: function () {
+        var self = this
+        axios.get('/invitations')
+          .then(function (response) {
+            var msg = _.get(response, 'data.message', false)
+            if (msg) {
+              self.snackbar.message = msg
+              self.snackbar.open = true
+            }
+            var invitations = _.get(response, 'data.invitations', false)
+            if (invitations) {
+              self.items = invitations
+            }
+            self.loading = false
+          })
+          .catch(function () {
+            self.loading = false
+            self.snackbar.message = 'Server Error.  Try refreshing page.'
+            self.snackbar.open = true
+        })
+      },
+      action: function (action, id) {
+        var self = this
+        axios.put(`/invitation/${action}/${id}`)
+          .then(function (response) {
+            var msg = _.get(response, 'data.message', false)
+            if (msg) {
+              self.snackbar.message = msg
+              self.snackbar.open = true
+            }
+            var invitation = _.get(response, 'data.invitation', false)
+            if (invitation) {
+              var index = _.findIndex(self.items, [ 'id', id ])
+              if (index >= 0) {
+                if (invitation.status === 1) { // recruiting is a status of 1
+                  self.items.splice(index, 1, invitation)
+                } else {
+                  self.items.splice(index, 1)
+                }
+              }
+            }
+            self.loading = false
+          })
+          .catch(function () {
+            self.loading = false
+            self.snackbar.message = 'Server Error.  Try refreshing page.'
+            self.snackbar.open = true
+        })
+      },
+      canAccept: function (id) {
+        var self = this
+        var item = self.getItem(id)
+        return !self.joined(item) && item.status === 1 // recruiting is a status 1
+      },
+      canDrop: function (id) {
+        var self = this
+        var item = self.getItem(id)
+        return self.joined(item) && item.status === 1 // recruiting is a status 1
+      },
+      joined: function (item) {
+        var self = this
+        return _.find(item.users, [ 'id', self.cu.id ])
+      },
+      getItem: function (id) {
+        var self = this
+        return _.find(self.items, [ 'id', id ])
+      },
+      publicPrivate: function (item) {
+        return item.public ? 'Public' : 'Private'
+      }
+    },
+    computed: {
+      cu: {
+        get: function () {
+          return this.$root.cu
+        },
+        set: function (value) {
+          this.$root.cu = value
+        }
+      },
+      snackbar: {
+        get: function () {
+          return this.$root.snackbar
+        },
+        set: function (value) {
+          this.$root.snackbar = value
+        }
+      },
+      nav: {
+        get: function () {
+          return this.$root.nav
+        },
+        set: function (value) {
+          this.$root.nav = value
+        }
+      },
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2, h3 {
+  font-weight: normal;
+}
+</style>
